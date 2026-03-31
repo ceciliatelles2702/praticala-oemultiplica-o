10c95a8efd1e7901fca9243075ba1f381f730f34
ceciliatelles2702 <ceciliatelles.jacome@gmail.com>
2026-03-30 21:40:45 -0300

    Add files via upload

---
+++ laço/src/laço/Laço.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laço;
+
+/**
+ *
+ * @author cecil
+ */
+import java.util.Scanner;
+public class Laço {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+        
+        int n;
+        
+        System.out.println(" Digite o número: ");
+        n = entrada.nextInt();
+        for(int i = 1; i <= n; i++) {
+            System.out.println(i*2);
+        }
+        }
+    }
+    
+
